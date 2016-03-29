bb7b01a59c4c135b2ba53c403aba4d438a89abbc
Viktor <qdeadyshka@yandex.ru>
2016-03-29 13:08:18 +0300

    Набросок блога.

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <?php
+$pdo =new PDO("mysql:host=localhost;dbname=blog;charset=utf8","root","vagrant");
+$statement =$pdo->query("select *from blog_data");
+?>
 
-while (true)
-{
-    $date = date('d-m-y');
-    $res = file_get_contents("./blog/{$date}.dat");
-    $glob =  glob('./blog/*.dat');
-    foreach ($glob as $item=>$value)
-    {
-    $handler = fopen("$value", 'r');
-        echo "Открыт файл:{$value}: ".PHP_EOL;
-    while (($str = fgets($handler))!==false)
-    {
-        echo $str;
-    }
-    fclose($handler);
-    }
-    //echo 'Введите запись в блог или введите \"exit\" чтобы выйти:';
-    $str = readline();
-    $date_note = date('h:i:s');
-    $res = 'Время: '.$date_note.' Сообщение: '.$str; //+ ' ' + $date_note
-    if ($str=='exit')
-        break;
-    file_put_contents("./blog/{$date}.dat",$res.PHP_EOL, FILE_APPEND);
-}
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Блог</title>
+    <div>
+        <form>
+            Заголовок<br>
+            <input type="text" name="title"><br>
+            Тело<br>
+            <input type="text" name="body" size="100"><br>
+             <input type="button" size="20" value="Запостить">
+        </form>
+    </div>
+    <div>
+    <?php
+        $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($row as $item=>$value)
+        {
+            echo "<h1>.{$value['title']}</h1><br><div>{$value['body']}</div><hr>";
+        }
+
+        ?>
+    </div>
+
+</head>
+</html>
+
 
