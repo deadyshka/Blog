a25549137ff156a3a417e713d821b4d730689b4d
Viktor <qdeadyshka@yandex.ru>
2016-03-29 16:21:04 +0300

    Дописал примитивный блог, впишет в базу. Пока таблица с юзерами не задействована, и нет защиты от mysql инъекций

--- index.php
+++ index.php
@@ -1,31 +1,48 @@
-<?php
-$pdo =new PDO("mysql:host=localhost;dbname=blog;charset=utf8","root","vagrant");
-$statement =$pdo->query("select *from blog_data");
-?>
-
-
 <html>
 <head>
     <meta charset="utf-8">
     <title>Блог</title>
     <div>
-        <form>
+        <form method="post">
             Заголовок<br>
             <input type="text" name="title"><br>
             Тело<br>
-            <input type="text" name="body" size="100"><br>
-             <input type="button" size="20" value="Запостить">
+            <textarea name="body" cols="100" rows="5">text test</textarea><br>
+             <input type="submit" value="Запостить">
         </form>
+        <?php
+        global $title;
+        global $body;
+        if (isset($_POST['title']))
+        {$title=$_POST['title'];}
+        if (isset($_POST['body']))
+        {$body=$_POST['body'];}
+        ?>
     </div>
     <div>
+
     <?php
-        $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $connection = new PDO("mysql:host=localhost;dbname=blog", 'root', 'vagrant', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+    ]);
+        $statement =$connection->query("select *from blog_data");
+        if (isset($title)&&isset($body)) {
+            $sql = "INSERT INTO blog_data(`title`,`body`, `autor_id`) VALUES ('$title', '{$body}', 2)";
+            if ($connection->query($sql))
+                echo "Записано в базу";
+            else
+                echo 'ошибка';
+        }
+        $row = $connection->query("SELECT `title`, `body` FROM blog_data ORDER BY `id` DESC");
+
         foreach ($row as $item=>$value)
         {
             echo "<h1>.{$value['title']}</h1><br><div>{$value['body']}</div><hr>";
         }
 
-        ?>
+    ?>
     </div>
 
 </head>
