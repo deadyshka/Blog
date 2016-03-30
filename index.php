06cd0350f8cddedcc3fb05c6aa8e746855f0f58e
Viktor <qdeadyshka@yandex.ru>
2016-03-30 21:14:20 +0300

    Защитился от SQL инъекций и XSS атак

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+$connection = new \PDO("mysql:host=localhost;dbname=blog", 'root', 'vagrant', [
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
+?>
+
 <html>
 <head>
     <meta charset="utf-8">
@@ -8,40 +16,38 @@
             <input type="text" name="title"><br>
             Тело<br>
             <textarea name="body" cols="100" rows="5"></textarea><br>
-             <input type="submit" value="Запостить">
+            <input type="submit" value="Запостить">
         </form>
-        <?php
-        global $title;
-        global $body;
-        if (isset($_POST['title']))
-        {$title=$_POST['title'];}
-        if (isset($_POST['body']))
-        {$body=$_POST['body'];}
-        ?>
     </div>
     <div>
 
-    <?php
-    $connection = new \PDO("mysql:host=localhost;dbname=blog", 'root', 'vagrant', [
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-    ]);
-        if (isset($title)&&isset($body)) {
-            $sql = "INSERT INTO blog_data(`title`,`body`, `autor_id`) VALUES ('$title', '{$body}', 2)";
-            if ($connection->query($sql))
+        <?php
+        // Начало метода добавления в базу данных ---------------------------------------------------------------
+        if (!empty($_POST['title']) && !empty($_POST['body'])) {
+
+            $sql = $connection->prepare("INSERT INTO blog_data(`title`,`body`, `autor_id`) VALUES (:_title, :_body, 2)");
+            if ($sql->execute([':_title'=>$_POST['title'], ':_body'=>$_POST['body']]))
                 echo "Записано в базу";
             else
                 echo 'ошибка';
         }
-        $row = $connection->query("SELECT `title`, `body` FROM blog_data ORDER BY `id` DESC");
-
-        foreach ($row as $item=>$value)
-        {
-            echo "<h1>{$value['title']}</h1><br><div>{$value['body']}</div><hr>";
-        }
-
-    ?>
+        $row = $connection->query("SELECT `title`, `body`, `created` FROM blog_data ORDER BY `id` DESC");
+        // конец метода добавления в базу данных ----------------------------------------------------------------
+        foreach ($row as $item => $value):
+            ?>
+            <h1>
+                <?= htmlspecialchars($value['title']); ?>
+            </h1>
+            <br>
+            <div>
+                <?= htmlspecialchars($value['body']); ?>
+            </div>
+            <br>
+            <div style="position: relative">
+            <i style="font-size: 12; text-align: center; background-color: lightgrey"><?= $value['created']; ?></i>
+            </div>
+            <hr>
+        <?php endforeach ?>
     </div>
 
 </head>
