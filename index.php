f3dd20879029f1fbb5c334a4645632da208f0706
Viktor <qdeadyshka@yandex.ru>
2016-04-13 16:02:46 +0300

    тест

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-
+//
 $connection = connection(['host' => 'localhost', 'dbname' => 'blog', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
 routes($_SERVER['REQUEST_URI'],[
     'Home' => 'Home',
